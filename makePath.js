f6a71fd26209a1dd13ef31a7b7c8cffe1536be62
Planetarium tech <planetarium_msk@icloud.com>
2022-07-12 18:40:33 +0300

    initial commit

---
+++ makePath.js
@@ -0,0 +1,8 @@
+﻿// Digistar Script 
+// Created: 29/04/2022
+
+function makePath() {
+	var folderName = Ds.GetObjectAttr('folderPath', 'text');
+	Ds.CreateObject('mySlides', 'slidesetClass');
+	Ds.SetObjectAttr('mySlides', 'path', '$Content/Shows/Tests/' + folderName);
+}
